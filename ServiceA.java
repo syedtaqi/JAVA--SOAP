c6d6a495ef50b53e64518307a0baa5eca07e6562
syedtaqi <raza.taqi4@gmail.com>
2017-01-29 21:10:44 -0500

    Update ServiceA.java

--- ServiceA.java
+++ ServiceA.java
@@ -3,7 +3,7 @@ package com.assignment.soap;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-//Author Syed Taqi Raza(100887193) - SOAP Assignment 3 - NET4001
+//Author Syed Taqi Raza - SOAP Assignment 3 - NET4001
 
 //Service Endpoint Interface
 public interface ServiceA extends Remote {
